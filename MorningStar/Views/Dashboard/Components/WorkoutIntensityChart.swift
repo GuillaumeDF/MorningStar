bed6b5dffbbe012e8e35dbce0c1853d20e95db81
Guillaume Djaider Fornari <guillaume.djaider.fornari@gmail.com>
2024-08-18 19:23:19 +0200

    initial draft of the WorkoutIntensityChart

---
+++ MorningStar/Views/Dashboard/Components/WorkoutIntensityChart.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutIntensityChart.swift
+//  MorningStar
+//
+//  Created by Guillaume Djaider Fornari on 15/08/2024.
+//
+
+import SwiftUI
+
+struct WorkoutIntensityChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutIntensityChart()
+}
---
+++ MorningStar/Views/Dashboard/Components/WorkoutIntensityView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutIntensityView.swift
+//  MorningStar
+//
+//  Created by Guillaume Djaider Fornari on 15/08/2024.
+//
+
+import SwiftUI
+
+struct WorkoutIntensityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutIntensityView()
+}
